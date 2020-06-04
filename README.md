38d1e730d233ce648e83ded730cb0062b7003b0b
Pelizari <claudiopelizari@gmail.com>
2020-06-03 23:33:13 -0300

    update aula 02

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 </h1>
 
 <h4 align="center"> 
-	:construction: Next Level Week 1.0 🚀 In progress.. :construction:
+	:construction: In progress.. :construction:
 </h4>
 <p align="center">
   <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/claudiopelizari/next-level-week?color=%2304D361">
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,31 @@
-import express from 'express';
+import express, { request, response } from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
+app.use(express.json());
+app.use(routes);
 
-    // JSON
+// rota: endereço completo da requisição
+// recurso: qual entidade estamos acessando do sistema
 
-    response.json([
-       'Diego',
-       'Cleiton',
-       'Robson',
-       'Daniel'
-    ]);
-});
+// GET: buscar uma ou mais informações do backend
+// POST: criar uma nova informação no backend
+// PUT: atualizar uma informação existente no backend
+// DELETE: remover uma informação do backend
+
+// POST http://localhost:3333/users = criar um usuário
+// GET http://localhost:3333/users = listar usuários
+// GET http://localhost:3333/users/5 = buscar dados do usuário com ID 5
+
+
+// request param: parâmetros que vem na própria rota que identificam um recurso
+// query param: parâmetros que vem na própria rota, geralmente opcionais para filtros e paginação
+// request body: parâmetros para criação/atualização de informações
+
+
+
+
 
 app.listen(3333);
 
